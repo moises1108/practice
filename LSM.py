c69bcc1ce20d5973ab9e01f9972da368f0b34d73
moises1108 <moises1108@gmail.com>
2022-10-05 06:31:17 +0000

    LMS

---
+++ LSM.py
@@ -0,0 +1,40 @@
+# Classes: Book (ID, isRented), User (ID, [rentedBooks]), Library ([{BooksAvail:1/0}])
+
+class Book():
+    def __init__(self, book_ID):
+        self.book_ID = book_ID
+        self.isRented = 0
+    
+class User():
+    def __init__(self,user_ID):
+        self.user_ID = user_ID
+        self.booksRented = []
+
+class Library():
+    def __init__(self, name, availableBooks):
+        self.name = name
+        self.availableBooks = availableBooks #[of objects]
+    
+    def checkBooks(self):
+        bookList = []
+        for b in self.availableBooks:
+            bookList.append(b.book_ID)
+        print(bookList)
+    
+
+
+
+book1 = Book(1) 
+book2 = Book(2)
+book3 = Book(3)
+
+user1 = User(10)
+user2 = User(20)
+user3 = User(30)
+
+library1 = Library('British Library',[book1,book2,book3])
+print(library1.availableBooks)
+library1.checkBooks()
+
+
+
